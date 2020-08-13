c9134dec1e0d2e5e9cdda6f16149f664cde3dd28
Prachi Phadke <prachi.phadke@microchip.com>
2020-08-13 12:32:50 +0530

    [SERCOM] Regenerated demo for sercom_usart_usart_echo_interrupt_pic32cm_mc00_curiosity_pro
    MH3-43690
    
    Signed-off-by: Prachi Phadke <prachi.phadke@microchip.com>

--- apps/sercom/usart/usart_echo_interrupt/firmware/src/config/pic32cm_mc00_curiosity_pro/peripheral/port/plib_port.h
+++ apps/sercom/usart/usart_echo_interrupt/firmware/src/config/pic32cm_mc00_curiosity_pro/peripheral/port/plib_port.h
@@ -65,18 +65,14 @@
 // *****************************************************************************
 // *****************************************************************************
 
-  
 /*** Macros for LED pin ***/
 #define LED_Set()               (PORT_REGS->GROUP[0].PORT_OUTSET = 1 << 24)
 #define LED_Clear()             (PORT_REGS->GROUP[0].PORT_OUTCLR = 1 << 24)
 #define LED_Toggle()            (PORT_REGS->GROUP[0].PORT_OUTTGL = 1 << 24)
-#define LED_Get()               (((PORT_REGS->GROUP[0].PORT_IN >> 24)) & 0x01)
 #define LED_OutputEnable()      (PORT_REGS->GROUP[0].PORT_DIRSET = 1 << 24)
 #define LED_InputEnable()       (PORT_REGS->GROUP[0].PORT_DIRCLR = 1 << 24)
+#define LED_Get()               (((PORT_REGS->GROUP[0].PORT_IN >> 24)) & 0x01)
 #define LED_PIN                  PORT_PIN_PA24
-
-
-
 // *****************************************************************************
 /* PORT Group
 
@@ -844,7 +840,7 @@ void PORT_GroupToggle(PORT_GROUP group, uint32_t mask);
     void PORT_GroupInputEnable(PORT_GROUP group, uint32_t mask)
 
   Summary:
-    Confgiures the selected IO pins of a group as input.
+    Configures the selected IO pins of a group as input.
 
   Description:
     This function configures the selected IO pins of a group as input. The pins
@@ -883,7 +879,7 @@ void PORT_GroupInputEnable(PORT_GROUP group, uint32_t mask);
     void PORT_GroupOutputEnable(PORT_GROUP group, uint32_t mask)
 
   Summary:
-    Confgiures the selected IO pins of a group as output.
+    Configures the selected IO pins of a group as output.
 
   Description:
     This function configures the selected IO pins of a group as output. The pins
