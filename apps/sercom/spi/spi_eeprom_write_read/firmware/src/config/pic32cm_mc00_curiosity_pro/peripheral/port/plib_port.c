4dcbd54ea88ad1840167af8874774d2a506fc6c0
Arpan Kumar <arpan.kumar@microchip.com>
2021-08-08 19:30:26 +0530

    [SERCOM] Regenerated demo for sercom_spi_spi_eeprom_write_read_pic32cm_mc00_curiosity_pro
    MH3-56948
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>

--- apps/sercom/spi/spi_eeprom_write_read/firmware/src/config/pic32cm_mc00_curiosity_pro/peripheral/port/plib_port.c
+++ apps/sercom/spi/spi_eeprom_write_read/firmware/src/config/pic32cm_mc00_curiosity_pro/peripheral/port/plib_port.c
@@ -74,9 +74,12 @@ void PORT_Initialize(void)
    /************************** GROUP 0 Initialization *************************/
    PORT_REGS->GROUP[0].PORT_DIR = 0x1008400;
    PORT_REGS->GROUP[0].PORT_OUT = 0x1008400;
+   PORT_REGS->GROUP[0].PORT_PINCFG[10] = 0x0;
+   PORT_REGS->GROUP[0].PORT_PINCFG[15] = 0x0;
    PORT_REGS->GROUP[0].PORT_PINCFG[16] = 0x1;
    PORT_REGS->GROUP[0].PORT_PINCFG[18] = 0x1;
    PORT_REGS->GROUP[0].PORT_PINCFG[19] = 0x1;
+   PORT_REGS->GROUP[0].PORT_PINCFG[24] = 0x0;
 
    PORT_REGS->GROUP[0].PORT_PMUX[8] = 0x2;
    PORT_REGS->GROUP[0].PORT_PMUX[9] = 0x22;
@@ -84,6 +87,7 @@ void PORT_Initialize(void)
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_DIR = 0x400000;
    PORT_REGS->GROUP[1].PORT_OUT = 0x400000;
+   PORT_REGS->GROUP[1].PORT_PINCFG[22] = 0x0;
 
 
 }
@@ -297,7 +301,7 @@ void PORT_PinPeripheralFunctionConfig(PORT_PIN pin, PERIPHERAL_FUNCTION function
     PORT_GROUP group = GET_PORT_GROUP(pin);
     uint32_t pin_num = ((uint32_t)pin) & 0x1FU;
     uint32_t pinmux_val = (uint32_t)((port_group_registers_t*)group)->PORT_PMUX[(pin_num >> 1)];
-    
+
     /* For odd pins */
     if (0U != (pin_num & 0x01U))
     {
@@ -308,7 +312,7 @@ void PORT_PinPeripheralFunctionConfig(PORT_PIN pin, PERIPHERAL_FUNCTION function
         pinmux_val = (pinmux_val & ~0x0FU) | periph_func;
     }
     ((port_group_registers_t*)group)->PORT_PMUX[(pin_num >> 1)] = (uint8_t)pinmux_val;
-    
+
     /* Enable peripheral control of the pin */
     ((port_group_registers_t*)group)->PORT_PINCFG[pin_num] |= (uint8_t)PORT_PINCFG_PMUXEN_Msk;
 }
