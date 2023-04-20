ee49d1692810ffc3d3d5f65db5c56dc4ae557098
Prachi Phadke <prachi.phadke@microchip.com>
2023-04-20 17:56:46 +0530

    [EIC] Regenerated demo for eic_eic_led_on_off_pic32cm_mc00_curiosity_pro
    MH3-77450

--- apps/eic/eic_led_on_off/firmware/src/config/pic32cm_mc00_curiosity_pro/peripheral/eic/plib_eic.h
+++ apps/eic/eic_led_on_off/firmware/src/config/pic32cm_mc00_curiosity_pro/peripheral/eic/plib_eic.h
@@ -96,14 +96,13 @@
     None.
 */
 
-typedef enum
-{
+
     /* External Interrupt Controller Pin 13 */
-    EIC_PIN_13 = 13,
+#define    EIC_PIN_13   (13U)
 
-    EIC_PIN_MAX = 16
+#define    EIC_PIN_MAX  (16U)
 
-} EIC_PIN;
+typedef uint16_t EIC_PIN;
 
 // *****************************************************************************
 /* EIC Interrupt Pin Callback Function Pointer Type
