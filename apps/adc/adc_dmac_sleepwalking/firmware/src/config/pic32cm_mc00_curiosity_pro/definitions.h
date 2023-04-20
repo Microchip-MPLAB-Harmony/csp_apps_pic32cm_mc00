8e4ed746963c516bc5be7356519bb4d0b12f84d5
Prachi Phadke <prachi.phadke@microchip.com>
2023-04-20 17:56:26 +0530

    [ADC] Regenerated demo for adc_adc_dmac_sleepwalking_pic32cm_mc00_curiosity_pro
    MH3-77438

--- apps/adc/adc_dmac_sleepwalking/firmware/src/config/pic32cm_mc00_curiosity_pro/definitions.h
+++ apps/adc/adc_dmac_sleepwalking/firmware/src/config/pic32cm_mc00_curiosity_pro/definitions.h
@@ -50,8 +50,8 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include "peripheral/nvmctrl/plib_nvmctrl.h"
-#include "peripheral/sercom/usart/plib_sercom0_usart.h"
 #include "peripheral/evsys/plib_evsys.h"
+#include "peripheral/sercom/usart/plib_sercom0_usart.h"
 #include "peripheral/adc/plib_adc0.h"
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
@@ -67,6 +67,12 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* Device Information */
+#define DEVICE_NAME			 "PIC32CM1216MC00048"
+#define DEVICE_ARCH			 "CORTEX-M0PLUS"
+#define DEVICE_FAMILY		 "PIC32CMMC"
+#define DEVICE_SERIES		 "PIC32CMMC00"
 
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 48000000
