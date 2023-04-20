e2ce0a5c6aefb6ee7b748b35dae8e9ab6c6f41f5
Prachi Phadke <prachi.phadke@microchip.com>
2023-04-20 17:56:39 +0530

    [DIVAS] Regenerated demo for divas_divas_c_operator_override_pic32cm_mc00_curiosity_pro
    MH3-77446

--- apps/divas/divas_c_operator_override/firmware/src/config/pic32cm_mc00_curiosity_pro/initialization.c
+++ apps/divas/divas_c_operator_override/firmware/src/config/pic32cm_mc00_curiosity_pro/initialization.c
@@ -54,7 +54,7 @@
 // ****************************************************************************
 // ****************************************************************************
 #pragma config NVMCTRL_BOOTPROT = SIZE_0BYTES
-#pragma config BODVDDUSERLEVEL = 0x0
+#pragma config BODVDDUSERLEVEL = 0x0U
 #pragma config BODVDD_DIS = ENABLED
 #pragma config BODVDD_ACTION = NONE
 #pragma config WDT_ENABLE = DISABLED
@@ -64,7 +64,7 @@
 #pragma config WDT_EWOFFSET = CYC8
 #pragma config WDT_WEN = DISABLED
 #pragma config BODVDD_HYST = DISABLED
-#pragma config NVMCTRL_REGION_LOCKS = 0xffff
+#pragma config NVMCTRL_REGION_LOCKS = 0xffffU
 
 
 
@@ -114,6 +114,8 @@
  ********************************************************************************/
 static void STDIO_BufferModeSet(void)
 {
+    /* MISRAC 2012 deviation block start */
+    /* MISRA C-2012 Rule 21.6 deviated 2 times in this file.  Deviation record ID -  H3_MISRAC_2012_R_21_6_DR_3 */
 
     /* Make stdin unbuffered */
     setbuf(stdin, NULL);
@@ -137,8 +139,10 @@ static void STDIO_BufferModeSet(void)
 
 void SYS_Initialize ( void* data )
 {
+    /* MISRAC 2012 deviation block start */
+    /* MISRA C-2012 Rule 2.2 deviated in this file.  Deviation record ID -  H3_MISRAC_2012_R_2_2_DR_1 */
 
-    NVMCTRL_REGS->NVMCTRL_CTRLB = NVMCTRL_CTRLB_RWS(3);
+    NVMCTRL_REGS->NVMCTRL_CTRLB = NVMCTRL_CTRLB_RWS(3U);
 
     STDIO_BufferModeSet();
 
@@ -164,6 +168,7 @@ void SYS_Initialize ( void* data )
 
     NVIC_Initialize();
 
+    /* MISRAC 2012 deviation block end */
 }
 
 
