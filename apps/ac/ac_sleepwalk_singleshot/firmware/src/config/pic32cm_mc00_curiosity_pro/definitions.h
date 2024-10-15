281bbd3ea88af6912948319ae6ad9f040d7bb7cc
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2024-10-15 12:37:36 +0530

    [AC] Regenerated demo for ac__ac_sleepwalk_singleshot__firmware__pic32cm_mc00_curiosity_pro.X
    MH3-99868

--- apps/ac/ac_sleepwalk_singleshot/firmware/src/config/pic32cm_mc00_curiosity_pro/definitions.h
+++ apps/ac/ac_sleepwalk_singleshot/firmware/src/config/pic32cm_mc00_curiosity_pro/definitions.h
@@ -50,14 +50,14 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include "peripheral/nvmctrl/plib_nvmctrl.h"
-#include "peripheral/evsys/plib_evsys.h"
 #include "peripheral/sercom/usart/plib_sercom0_usart.h"
+#include "peripheral/evsys/plib_evsys.h"
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/pm/plib_pm.h"
-#include "peripheral/ac/plib_ac.h"
 #include "peripheral/rtc/plib_rtc.h"
+#include "peripheral/ac/plib_ac.h"
 
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
@@ -68,13 +68,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "PIC32CM1216MC00048"
-#define DEVICE_ARCH			 "CORTEX-M0PLUS"
-#define DEVICE_FAMILY		 "PIC32CMMC"
-#define DEVICE_SERIES		 "PIC32CMMC00"
+#define DEVICE_NAME          "PIC32CM1216MC00048"
+#define DEVICE_ARCH          "CORTEX-M0PLUS"
+#define DEVICE_FAMILY        "PIC32CMMC"
+#define DEVICE_SERIES        "PIC32CMMC00"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 48000000
+#define CPU_CLOCK_FREQUENCY 48000000U
 
 // *****************************************************************************
 // *****************************************************************************
